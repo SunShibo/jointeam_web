03e781f0688254d4e0c55245c72733433720f34c
zhaizhangxin <1425528308@qq.com>
2020-06-05 01:42:43 +0800

    login提交

--- js/index.js
+++ js/index.js
@@ -3,18 +3,63 @@ new Vue({
 	data() {
 		return {
 			activeIndex: '1',
-			bannerUrl: [{
-				url: 'images/banner_2.png'
-			}, {
-				url: 'images/banner_3.png'
-			}, {
-				url: 'images/banner_4.png'
-			}]
+			bannerUrl: [],
+			industry: {
+				title: '',
+				image: '',
+				introduction: '',
+				source: '',
+				updateTime: ''
+			}
 		};
 	},
 	methods: {
 		handleSelect(key, keyPath) {
 			console.log(key, keyPath);
-		}
+		},
+		loadBanner(){
+			var that = this;
+			axios.post(url + '/banner/selectAll')
+				.then(function(res) {
+//					console.log(res);
+					if(res.success = true) {
+						that.bannerUrl = res.data.data;
+					} else {
+						alert(res.msg)
+					}
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		},
+		loadData() {
+			var that = this;
+			axios.post(url + '/main/meau')
+				.then(function(res) {
+					console.log(res);
+					if(res.success = true) {
+						let dataTime = res.data.data.industry.list.updateTime;
+						let times = that.unixTimeToDateTime(dataTime);
+						res.data.data.industry.list['updateTime'] = times;
+						that.industry = res.data.data.industry.list;
+					} else {
+						alert(res.msg)
+					}
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		},
+		unixTimeToDateTime(unixtime) {
+			var now = new Date(unixtime * 1000); // 依情况进行更改 * 1
+			y = now.getFullYear();
+			m = now.getMonth() + 1;
+			d = now.getDate();
+			return (m < 10 ? "0" + m : m) + "/" + (d < 10 ? "0" + d : d) + " " + now.toTimeString().substr(0, 8);
+		},
+	},
+	mounted() {
+		this.loadBanner();
+		this.loadData();
 	}
 }).$mount('#app')
--- js/login.js
+++ js/login.js
@@ -18,39 +18,40 @@ var cont = new Vue({
 			//正则表达式，十一位数字的电话号码
 			var phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
 			//如果手机号码输入为空，则再输入框后插入错误信息
-			if (phone == '') {
+			if(phone == '') {
 				alert('手机号码不能为空！');
 				return false;
-			} else if (password == '') {
+			} else if(password == '') {
 				alert('密码不能为空！');
 				return false;
-			} else if (!phoneReg.test(phone)) {
+			} else if(!phoneReg.test(phone)) {
 				alert('请输入正确的手机号码');
 				return false;
 			} else {
 				var strPhone = phone;
 				var strPass = password;
 				localStorage.setItem('strPhone', strPhone);
-				if (this.checked == true) {
+				if(this.checked == true) {
 					localStorage.setItem('strPass', strPass);
 				} else {
 					localStorage.removeItem('strPass');
 				}
-				$.ajax({
-					url: url + '/user/loginByPwd',
-					data:{
-						phone:phone,
-						password:password
-					},
-					dataType: "JSON",
-					type: "POST",
-					success: function(res) {
-						console.log(res)
-						if(res.code = 00000){
-							// window.location.href='./index.html'
+				axios.post(url + '/user/loginByPwd', {
+						phone: phone,
+						password: password
+					})
+					.then(function(res) {
+						localStorage.setItem("cookie", document.cookie);
+
+						if(res.success = true) {
+							 window.location.href='./index.html'
+						} else {
+							alert(res.msg)
 						}
-					}
-				})
+					})
+					.catch(function(error) {
+						console.log(error);
+					});
 			}
 		}
 	},
@@ -62,10 +63,10 @@ var cont = new Vue({
 $(document).ready(function() {
 	var strPhone = localStorage.getItem('strPhone');
 	var strPass = localStorage.getItem('strPass');
-	if (strPhone) {
+	if(strPhone) {
 		cont.phone = strPhone;
 	}
-	if (strPass) {
+	if(strPass) {
 		cont.password = strPass;
 	}
 });
